d1190cbe490db7df33f66706abb5e536d219d5aa
scullionw <williamscullion@gmail.com>
2019-09-26 22:13:42 -0400

    more hygienic

--- binpack-macros/src/lib.rs
+++ binpack-macros/src/lib.rs
@@ -4,27 +4,34 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(Bundle)]
+#[proc_macro_derive(Bundle, attributes(folder))]
 pub fn bundle(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let name = &input.ident;
 
+    let window_mode = if cfg!(feature = "nowindow") {
+        quote! {
+            const CREATE_NO_WINDOW: u32 = 0x08000000;
+            cmd.creation_flags(CREATE_NO_WINDOW);
+        }
+    } else {
+        quote! {}
+    };
+
     let expanded = quote! {
-        // #(#attrs)*
-        use rust_embed::RustEmbed;
 
-        #[derive(RustEmbed)]
+        #[derive(rust_embed::RustEmbed)]
         #[folder = "dist/"]
-        struct Asset;
+        struct __Asset;
 
 
-        struct Packed {
+        struct __Packed {
             temp_dir: tempfile::TempDir,
             exe_path: ::std::option::Option<::std::path::PathBuf>,
         }
 
-        impl Packed {
+        impl __Packed {
             fn new() -> Self {
                 Self {
                     temp_dir: tempfile::TempDir::new().expect("Could not create temp dir"),
@@ -34,7 +41,7 @@ pub fn bundle(input: TokenStream) -> TokenStream {
 
             fn dump(&mut self) {
 
-                for file in Asset::iter() {
+                for file in __Asset::iter() {
                     let file_path = ::std::path::Path::new(file.as_ref());
 
                     if file_path.extension().expect("Could not get filename") == "exe" {
@@ -50,16 +57,16 @@ pub fn bundle(input: TokenStream) -> TokenStream {
                             .expect("Could not create dirs recursively for embedded files");
                     }
 
-                    let data = Asset::get(file_path.to_str().expect("File path is not unicode"))
+                    let data = __Asset::get(file_path.to_str().expect("File path is not unicode"))
                         .expect("Could not get the asset");
 
                     ::std::fs::write(path, data).expect("Writing in temp directory failed");
                 }
             }
 
-            fn launch(&self, no_window: bool) {
+            fn launch(&self) {
                 if let Some(exe_path) = &self.exe_path {
-                    execute(self.temp_dir.path(), exe_path, no_window);
+                    __execute(self.temp_dir.path(), exe_path);
                 } else {
                     eprintln!("No executable found!");
                 }
@@ -68,27 +75,19 @@ pub fn bundle(input: TokenStream) -> TokenStream {
 
         impl #name {
             fn run() {
-                let mut packed = Packed::new();
+                let mut packed = __Packed::new();
                 packed.dump();
-                packed.launch(false);
-            }
-            fn run_no_window() {
-                let mut packed = Packed::new();
-                packed.dump();
-                packed.launch(true);
+                packed.launch();
             }
         }
 
-        fn execute(temp_dir: &::std::path::Path, exe_path: &::std::path::Path, no_window: bool) {
-            use std::os::windows::process::CommandExt;
-            const CREATE_NO_WINDOW: u32 = 0x08000000;
+        fn __execute(temp_dir: &::std::path::Path, exe_path: &::std::path::Path) {
+            use ::std::os::windows::process::CommandExt;
 
             ::std::env::set_current_dir(temp_dir).expect("Could not change directory");
             let mut cmd = ::std::process::Command::new(exe_path);
 
-            if no_window {
-                cmd.creation_flags(CREATE_NO_WINDOW);
-            }
+            #window_mode
 
             let mut child = cmd.spawn().expect("Could not spawn command");
             child.wait().expect("command wasn't running");
